a7fb36909ffc3f960d88690ebff4027b28c5b231
Chris Concannon <concach00@gmail.com>
2016-11-08 10:54:22 -0700

    iteration 4 complete and tested

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
       redirect_to root_path
     else
       flash[:alert] = "Login Error"
-      redirect_to new_user_path
+      redirect_to new_session_path
     end
   end
 
