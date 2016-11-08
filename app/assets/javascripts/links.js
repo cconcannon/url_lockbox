92d381f2643226ccce2cc847536e244102eb9d95
Chris Concannon <concach00@gmail.com>
2016-11-08 13:28:17 -0700

    alphabetical sorting works

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -12,4 +12,31 @@ $(document).ready(function() {
       }
     });
   });
+
+  $('#link_sort').click(function() {
+    $(this).toggleClass("hidden");
+    $('#link_unsort').toggleClass("hidden");
+    var table = $('table');
+    var rows = $links.get();
+    sorted = rows.sort(function(a, b) {
+      if ($(a).data("title") > $(b).data("title")) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    $.each(sorted, function(index, row) {
+      table.children('tbody').append(row);
+    });
+  });
+
+  $('#link_unsort').click(function() {
+    $(this).toggleClass("hidden");
+    $('#link_sort').toggleClass("hidden");
+    var table = $('table');
+    var rows = $links.get();
+    $.each(rows, function(index, row) {
+      table.children('tbody').append(row);
+    });
+  });
 });
