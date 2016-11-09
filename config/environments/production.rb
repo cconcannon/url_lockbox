03d75be9497d1b3cf0354fd7df3c47192a7569fa
Chris Concannon <concach00@gmail.com>
2016-11-08 17:43:16 -0700

    comma missing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => "gmail.com"
+    :domain               => "gmail.com",
     :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
     :authentication       => "plain",
