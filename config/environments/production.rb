7a56fc5f3ba9f0cf4eacae05f73727c0b6debe12
Chris Concannon <concach00@gmail.com>
2016-11-08 17:54:06 -0700

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "url_lockbox_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => "gmail.com",
